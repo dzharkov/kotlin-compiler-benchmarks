f4b9e785aaea8b5167fb56f9ebb3cbbd913dd1ae
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-10-17 18:47:02 +0300

    Clear file content temporary

--- src/main/kotlin/org/jetbrains/kotlin/benchmarks/ReportParser.kt
+++ src/main/kotlin/org/jetbrains/kotlin/benchmarks/ReportParser.kt
@@ -1,31 +1,31 @@
 package org.jetbrains.kotlin.benchmarks
 
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.File
-
-data class Report(
-        val name: String,
-        val parameters: String,
-        val score: Double,
-        val deviation: Double
-)
-
-fun parseFile(fileName: String) =
-        JSONArray(File(fileName).readText()).map {
-            it as JSONObject
-            val size = it.getJSONObject("params").getString("size").toInt()
-            val score = it.getJSONObject("primaryMetric").getDouble("score")
-            val scoreError = it.getJSONObject("primaryMetric").getDouble("scoreError")
-
-            val name =
-                    it.getString("benchmark")
-                            .replace("org.jetbrains.kotlin.benchmarks.", "")
-            Report(name, size.toString(), score, scoreError)
-        }
-
-fun main(args: Array<String>) {
-    parseFile("/home/sufix/work/compiler-benchmarks/reports/benchmarks-basic-inference-15-03-2017.json").forEach {
-        println("${it.name} size=${it.parameters} score=${it.score} +-${it.deviation}")
-    }
-}
+//import org.json.JSONArray
+//import org.json.JSONObject
+//import java.io.File
+//
+//data class Report(
+//        val name: String,
+//        val parameters: String,
+//        val score: Double,
+//        val deviation: Double
+//)
+//
+//fun parseFile(fileName: String) =
+//        JSONArray(File(fileName).readText()).map {
+//            it as JSONObject
+//            val size = it.getJSONObject("params").getString("size").toInt()
+//            val score = it.getJSONObject("primaryMetric").getDouble("score")
+//            val scoreError = it.getJSONObject("primaryMetric").getDouble("scoreError")
+//
+//            val name =
+//                    it.getString("benchmark")
+//                            .replace("org.jetbrains.kotlin.benchmarks.", "")
+//            Report(name, size.toString(), score, scoreError)
+//        }
+//
+//fun main(args: Array<String>) {
+//    parseFile("/home/sufix/work/compiler-benchmarks/reports/benchmarks-basic-inference-15-03-2017.json").forEach {
+//        println("${it.name} size=${it.parameters} score=${it.score} +-${it.deviation}")
+//    }
+//}

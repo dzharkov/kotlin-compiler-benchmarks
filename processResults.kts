a4c683c79eb4f183ac809f693176d92571e6ad2f
Denis Zharkov <denis.zharkov@jetbrains.com>
2019-05-31 12:48:26 +0300

    Fix results parsing

--- processResults.kts
+++ processResults.kts
@@ -7,7 +7,7 @@ while (true) {
     // Benchmark                                      (isIR)  (size)  (useNI)  Mode  Cnt   Score   Error  Units
     // InferenceFromArgumentCallsBenchmark.benchmark   false       1    false  avgt       14.208
     val (isIR, size, isNI, result) =
-            ".*(true|false)\\s+(\\d+)\\s+(true|false)\\s+avgt\\s+(?:\\d+)?\\s+(\\d+\\.\\d+).*"
+            ".*(true|false)\\s+(\\d+)\\s+(true|false)?\\s+avgt\\s+(?:\\d+)?\\s+(\\d+\\.\\d+).*"
                     .toRegex().matchEntire(line)?.destructured ?: continue
 
     val irPostfix = if (isIR.toBoolean()) " isIR=true" else ""
